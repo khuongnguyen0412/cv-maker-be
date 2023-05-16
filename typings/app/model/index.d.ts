19f34dee4c82670273d620e583ea3a9edb8ab82e
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2023-05-16 10:31:26 +0700

    Add typing files

--- typings/app/model/index.d.ts
+++ typings/app/model/index.d.ts
@@ -2,10 +2,14 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+import ExportCv from '../../../app/model/cv';
+import ExportTemplate from '../../../app/model/template';
 import ExportUser from '../../../app/model/user';
 
 declare module 'egg' {
   interface IModel {
+    Cv: InstanceType<ExportCv>;
+    Template: InstanceType<ExportTemplate>;
     User: InstanceType<ExportUser>;
   }
 }
